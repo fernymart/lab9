ec3bba92dbf99b116e25a38f50e186b322bbd208
Fernando Martínez Ortiz <fermtz020700@hotmail.com>
2021-11-16 22:38:07 -0600

    Finished edit and delete functionality

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -22,19 +22,30 @@ router.post('/newPost', async (req,res) =>{
   res.redirect('/')
 })
 
-router.post('/edit', async (req,res) =>{
-
+router.post('/edit/:id', async (req,res) =>{
+  var id = req.params.id
+  console.log(id)
+  await Post.updateOne({_id: id}, req.body);
+  res.redirect('/')
 })
-router.get('/edit:id', async (req,res) =>{
-  
+router.get('/edit/:id', async (req,res) =>{
+  var id = req.params.id
+  console.log(id)
+  const editPost = await Post.findById(id);
+  res.render('edit.ejs', {editPost})
 })
 
-router.post('/delete', async (req,res) =>{
-
+router.post('/delete/:id', async (req,res) =>{
+  var id = req.params.id
+  await Post.remove({_id: id})
+  res.redirect('/')
 })
 
-router.get('/delete', async (req,res) =>{
-  
+router.get('/delete/:id', async (req,res) =>{
+  var id = req.params.id
+  console.log(id)
+  const delPost = await Post.findById(id);
+  res.render('delete.ejs', {delPost})
 })
 
 module.exports = router;
